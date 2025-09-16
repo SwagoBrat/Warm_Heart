77b484642c9ef9aaa2bac2da861263fb0eac2252
Timur <timurcernitskiy@gmail.com>
2025-09-17 00:52:56 +0300

    End the second section

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,36 @@
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import "/src/sass/style.scss";
+
+try {
+    new Swiper(".popular__slider", {
+        slidesPerView: 1,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".icon-right-open",
+            prevEl: ".icon-left-open",
+        },
+        breakpoints: {
+            480: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            1000: {
+                spaceBetween: 20,
+                slidesPerView: 3,
+            },
+            1920: {
+                spaceBetween: 30
+            }
+        },
+        modules: [Navigation, Pagination],
+    });
+} catch (e) { }
