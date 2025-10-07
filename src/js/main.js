d99f011dcd540f4fb0476494672c8b4d8cf29aa4
Timur <timurcernitskiy@gmail.com>
2025-10-07 17:04:44 +0300

    end first page, add burger, start new page

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,17 @@ import "swiper/css/pagination";
 import "/src/sass/style.scss";
 
 
+const menu = document.querySelector(".header__menu"),
+    burger = document.querySelector(".header__burger"),
+    close = document.querySelector(".header__menu-close");
+
+burger.addEventListener("click", () => {
+    menu.classList.add("header__menu-active")
+})
+
+close.addEventListener("click", () => {
+    menu.classList.remove("header__menu-active")
+})
 
 try {
     new Swiper(".popular__slider", {
@@ -53,3 +64,50 @@ try {
         });
     });
 } catch (e) { }
+
+try {
+    let tabsContent = document.querySelectorAll(".photo img"),
+        tabsParent = document.querySelector('.info__pagenation'),
+        tabsItems = document.querySelectorAll('.info__pagenations');
+
+    function hideTabs() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide')
+            item.classList.remove('active', 'fade')
+
+        });
+
+        tabsItems.forEach(item => {
+            item.classList.remove('info__pagenations-active')
+        })
+    }
+
+
+    function showTabs(i = 0, withFade = true) {
+        tabsContent[i].classList.add('active');
+        tabsContent[i].classList.remove('hide');
+
+        if (withFade) {
+            tabsContent[i].classList.add('fade');
+        }
+
+        tabsItems[i].classList.add("info__pagenations-active");
+    }
+
+    hideTabs();
+    showTabs(0, false);
+
+    tabsParent.addEventListener('click', function (event) {
+        const target = event.target;
+
+        if (target) {
+            tabsItems.forEach((item, i) => {
+                if (target == item) {
+                    hideTabs();
+                    showTabs(i); // тут fade спрацює
+                }
+            });
+        }
+    });
+} catch (e) { }
+
